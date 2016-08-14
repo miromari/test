67cf0bd71ecc70707c655184b50104c573c7076c
–Ь–∞—А–Є—П –Ь–Є—А–Њ—И–љ–Є—З–µ–љ–Ї–Њ <marg13@yandex.ru>
2016-08-14 14:27:19 +0300

    Initial Commit

---
+++ Lesson1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson1
+//
+//  Created by Мария Мирошниченко on 14.08.16.
+//  Copyright © 2016 GB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson1
+//
+//  Created by Мария Мирошниченко on 14.08.16.
+//  Copyright © 2016 GB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
